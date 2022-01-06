f21472040636ea480b5a411170b56506b7302446
Neto Martins <netomartins2@hotmail.com>
2022-01-06 00:39:49 -0300

    chore: Salvando o voto e finalizando

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let numeros = document.querySelector('.d-1-3');
 let etapaAtual = 0;
 let numero = '';
 let votoBranco = false;
+let votos = [];
 
 function comecarEtapa() {
   let etapa = etapas[etapaAtual];
@@ -93,8 +94,7 @@ function branco() {
 }
 
 function corrige() {
-    comecarEtapa();
-  
+  comecarEtapa();
 }
 
 function confirma() {
@@ -104,10 +104,17 @@ function confirma() {
 
   if (votoBranco === true) {
     votoConfirmado = true;
-    console.log('confirmando como branco');
+    votos.push({
+      etapa: etapas[etapaAtual].titulo,
+      voto: 'branco'
+    });
+
   } else if (numero.length === etapa.numeros) {
     votoConfirmado = true;
-    console.log('confirmando como' + numero);
+    votos.push({
+      etapa: etapas[etapaAtual].titulo,
+      voto: numero
+    });
   }
 
   if (votoConfirmado) {
@@ -115,9 +122,13 @@ function confirma() {
     if (etapas[etapaAtual] !== undefined) {
       comecarEtapa();
     } else {
-      console.log('FIM!');
+      document.querySelector('.tela').innerHTML = `<div class="aviso--gigante pisca">FIM!</div>`;
+      console.log(votos);
+      
+      }
+      
     }
   }
-}
+
 
 comecarEtapa();
